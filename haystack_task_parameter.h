26182b67934dbe969e79e418c09f643b45e9e794
duanhaisheng <duanhaisheng@gmail.com>
2018-05-09 19:28:47 +0800

    Implemented Haystack\\Task\\Parameter

--- haystack_task_parameter.h
+++ haystack_task_parameter.h
@@ -1,6 +1,8 @@
 #ifndef HAYSTACK_TASK_PARAMETER
 #define HAYSTACK_TASK_PARAMETER
 
+#define HAYSTACK_TASK_PARAMETER_PROPERTY_VALUES "values"
+
 extern zend_class_entry *haystack_task_parameter_ce;
 
 #endif
